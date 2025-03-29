fc92e623d305bf78cc408596e8f21a3d6f979309
lks1996 <mosplee@naver.com>
2025-03-29 20:07:42 +0900

    수정

--- start.sh
+++ start.sh
@@ -3,11 +3,16 @@
 # Start scrap-by-newsletter.js
 node --experimental-fetch scrap-by-newsletter.js
 
+# Sleep for 5 seconds to ensure the server is ready
 sleep 5
 
-# call API
+# Now, call the API and wait for it to complete
 curl -X GET http://localhost:3000/scrape-emails
 
+# Wait until the previous process completes
+wait $!
+
+# Sleep for 15 seconds before starting the next process
 sleep 15
 
 # Start scrap-organizer.js
