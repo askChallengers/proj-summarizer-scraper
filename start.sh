60b4b2aa11b79c01ad2be181751f75b3b62938f0
lks1996 <mosplee@naver.com>
2025-04-04 20:55:19 +0900

    sh 실행 파일 수정

--- start.sh
+++ start.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 
 # Start scrap-by-newsletter.js
-node scrap-by-newsletter.js &
-SCRAP_BY_NEWSLETTER_PID=$!
+node scrap-by-newsletter.js
 
 # Sleep for 5 seconds to ensure the server is ready
 sleep 5
 
-# Now, call the API and wait for it to complete
-curl -X GET http://localhost:3000/scrape-emails
-
 # Sleep for 15 seconds before starting the next process
 sleep 15
-
-# Kill the scrap-by-newsletter.js process
-kill $SCRAP_BY_NEWSLETTER_PID
 
 # Start scrap-organizer.js
 node scrap-organizer.js
